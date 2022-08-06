e3b2262b35ed8ff31cd74c2e4852843714ee5c0e
Suman Islam <sumanislam1416@gmail.com>
2022-08-06 18:05:19 +0600

    add homepage

---
+++ client/src/component/header/header.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav className="container d-flex ">
+      div
+    </nav>
+  );
+};
+
+export default Header;
---
+++ client/src/page/homepage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Homepage = () => {
+  return(
+    <h1>homepage</h1>
+  )
+};
+
+export default Homepage;
