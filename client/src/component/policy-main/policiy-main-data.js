eeb44432daa59b6be0d33312e75dce9248d0a83b
Suman Islam <sumanislam1416@gmail.com>
2022-08-08 01:44:39 +0600

    create and complete policy main component

---
+++ client/src/component/policy-main/policiy-main-data.js
@@ -0,0 +1,26 @@
+export const policyData = [
+	{
+		text: 'Anyone can visit our site and see bio-data. Registration or payment will not be required for this. You have to do is register and fill out the form if you want to create your bio-data on our site.',
+	},
+	{
+		text: 'To create a bio-data on our site you need to provide all personal and family details including name, email address, mobile number, location, etc. We will collect and verify this information to approve your bio-data. Protecting the privacy of your information is our priority.',
+	},
+	{
+		text: 'Your personal information is used to make a complete bio-data which is associated with the profile you have created, to allow other members to find a suitable bio-data through a search but your identifiable information is hidden to the visitors on our site. Your information will be sorted out and presented on our site in such a way that your identity must be kept secret from the visitors.',
+	},
+	{
+		text: 'The information we collect from our users is not shared with any individual or organization without the user’s approval. We do not sell, rent, or loan any identifiable information at the individual level regarding its customers to any third party. We will only provide contact information to an investigator when you permit us to share it. However, ordhekdeen.com reserves the right to provide the user’s contact information (user’s email address & guardian’s phone number) to the investigator without the permission of the user. At the same time, the data of the investigator will be stored.',
+	},
+	{
+		text: 'Payment is required when someone wants to communicate with the parents of any specific profile (bio-data).',
+	},
+	{
+		text: 'You can delete or hide your bio-data by contacting us.',
+	},
+	{
+		text: 'To get the contact number of the parents of any profile (bio-data) you have to fill in the application form and make the required payment. After getting permission from the other party, we will provide you the contact number within 48 hours except holidays.',
+	},
+	{
+		text: 'We do not promote any third-party advertisement at this moment.',
+	},
+];
