50cee699816c354d7142ae8e17236c5d7a003065
Suman Islam <sumanislam1416@gmail.com>
2022-08-07 17:54:37 +0600

    finish footer component

---
+++ client/src/component/footer/footer.component.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+// styles
+import './footer.style.scss';
+
+const Footer = () => {
+  return (
+		<div className='bg-purple' id='footer'>
+			<div className='container pt-5'>
+				<hr />
+				<div className='d-flex justify-content-evenly'>
+					<p>কপিরাইট ©2022 doortonikah.com। সর্বস্বত্ব সংরক্ষিত।</p>
+					<div className='mt-3'>
+						<a href='#' className='mr-3 text-light'>
+							প্রাইভেসি পলিসি
+						</a>
+						<a href='#' className='text-light'>
+							শর্তাবলি
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Footer;
--- client/src/page/homepage.jsx
+++ client/src/page/homepage.jsx
@@ -3,12 +3,14 @@ import React from "react";
 // components
 import Header from "../component/header/header.component";
 import LifePartnerSelectionNasihaCard from "../component/life-partner-selection-nasiha/life-partner-selection-nasiha-card.component";
+import Footer from "../component/footer/footer.component";
 
 const Homepage = () => {
   return(
     <>
       <Header />
       <LifePartnerSelectionNasihaCard />
+      <Footer />
     </>
   )
 };
