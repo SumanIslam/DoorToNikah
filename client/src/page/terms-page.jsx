0dcdb6805e8a0fd27716ca372226053ed9c0d228
Suman Islam <sumanislam1416@gmail.com>
2022-08-08 12:42:15 +0600

    complete terms page

--- client/src/page/terms-page.jsx
+++ client/src/page/terms-page.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 
 // component
 import Header from '../component/header/header.component';
+import TitleBanner from '../component/title-banner/title-banner.component';
+import TermsMain from '../component/terms-main/terms-main.component';
+import Footer from '../component/footer/footer.component';
 
 const TermsCondition = () => {
-	return <Header />;
+	
+	return (
+		<>
+			<Header />
+			<TitleBanner title='শর্তাবলি' />
+      <TermsMain />
+      <Footer />
+		</>
+	);
 };
 
 export default TermsCondition;
