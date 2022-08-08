20e5cb812c6c22e47537de4b0b3406b7f4d93c72
Suman Islam <sumanislam1416@gmail.com>
2022-08-08 12:41:42 +0600

    complete termsMain component

---
+++ client/src/component/terms-main/terms-main-data.js
@@ -0,0 +1,92 @@
+export const generalText = [
+	{
+    id: 1,
+		text: 'Welcome to doortonikah.com. It is a marital platform basically focused on Islamic Sharia. Our main goal is to provide the highest level of assistance in finding a religious life partner through this platform. We want to build a comprehensive platform for marriage according to Islamic Shariah, through which the path of marriage will become a little easier, InshaAllah.',
+	},
+	{
+    id: 2,
+		text: 'doortonikah.com is intended only to serve as a preliminary medium of contact between the two parties (bride &amp; groom) who have a decisive intention to enter into a matrimonial alliance. It is by no means a dating/contact website. We do not monitor first and subsequent contacts, introductions, and interactions which may take place among the visitors or members. We are not involved in any further interactions between the two parties after providing contact information.',
+	},
+];
+
+export const termsWithTitle = [
+	{
+		id: 1,
+		title: 'Eligibility',
+		text: 'To register as a member of doortonikah.com, you must be a Muslim (practicing Muslim) of legal marriageable age. This site is only to facilitate lawful marriages between persons who are legally competent to enter into matrimony under the laws to which they are subjected. By using this site, you concede that you have the right, authority, and legal capacity to enter into this agreement that you are not prohibited or prevented by any applicable law for the time being in force or any order or decree or injunction from any court, tribunal or any such competent authority restraining you from entering into matrimony. Membership in the Service is void where prohibited. You must agree to abide by all of the terms and conditions of this agreement.',
+	},
+	{
+		id: 2,
+		title: 'Terms of Use',
+		text: 'Membership in doortonikah.com is for your sole personal use. Your membership in doortonikah.com is not transferable to any person or entity. While using the site you remain personally responsible for maintaining the confidentiality of your account and password and for restricting access to your computer and you agree to accept responsibility for all activities that occur under your account or password',
+	},
+	{
+		id: 3,
+		title: 'Termination',
+		text: 'You can terminate your membership at any time, for any reason by writing to doortonikah.com. You can also hide your bio-data on this site by writing to us.',
+	},
+	{
+		id: 4,
+		title: 'No Commercial Use by Members',
+		text: 'This site is for the personal use of individual members to register their profiles and find suitable matches but cannot be used in connection with any other commercial endeavors.',
+	},
+	{
+		id: 5,
+		title: 'Proprietary Rights',
+		text: 'doortonikah.com owns and retains all proprietary rights in the doortonikah.com Site. The Site contains the copyrighted material, trademarks, and other proprietary information of doortonikah.com and its licensors.',
+	},
+	{
+		id: 6,
+		title: 'Copyright Policy',
+		text: 'You cannot post, distribute or reproduce in any way any copyrighted material, trademarks, or other proprietary information without obtaining the prior written consent of the owner of such proprietary rights.',
+	},
+	{
+		id: 7,
+		title: 'Privacy',
+		text: 'The use of doortonikah.com is governed by our <a href="/privacy&policy" className="text-purple">privacy policy</a>.',
+	},
+	{
+		id: 8,
+		title: 'Disclaimer',
+		text: 'doortonikah.com is not responsible for any false or inaccurate information given by any member on this site. Members complete their bio-data on our site with the assurance of providing genuine information. We are not by any means involved in any subsequent interaction or activity of members after providing the contact information of the member. The members or visitors themselves are fully responsible for verifying the information provided in a bio-data after contact.',
+	},
+];
+
+export const termsWithBullet = [
+	{
+		id: 1,
+		title: 'Payment Policy',
+		list: [
+			{
+				id: 1,
+				text: 'You will have to pay us to get the contact information (user’s email address & guardian’s phone number) of a particular user (bio-data). You don’t need to pay for anything other than this service.',
+			},
+			{
+				id: 2,
+				text: 'You will have to pay us to get the contact information (user’s email address & guardian’s phone number) of a particular user (bio-data). You don’t need to pay for anything other than this service.',
+			},
+		],
+	},
+	{
+		id: 2,
+		title: 'Other Terms of Use',
+		list: [
+			{
+				id: 1,
+				text: 'doortonikah.com reserves the right in its sole discretion to review the activity & status of each account and block the account of a member based on such review.',
+			},
+			{
+				id: 2,
+				text: 'Multiple profiles of the same person are not allowed on OrdhekDeen.com. We reserve the right to deactivate all multiple profiles without any notice.',
+			},
+			{
+				id: 3,
+				text: 'In order to protect doortonikah.com and/or our members from any abuse, doortonikah.com reserves the right to take appropriate steps as it deems appropriate in its sole discretion in order to protect the interest of other members.',
+			},
+			{
+				id: 4,
+				text: 'We usually provide contact information to an investigator when you give us permission to share it. However, doortonikah.com reserves the right to provide the user’s contact information (user’s email address & guardian’s phone number) to the investigator without the permission of the user.',
+			},
+		],
+	},
+];
