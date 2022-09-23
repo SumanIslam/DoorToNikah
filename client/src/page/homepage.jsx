b96a54eb26e280363fbc6ddd3e48d9480e0c5674
Suman Islam <sumanislam1416@gmail.com>
2022-09-23 06:55:43 +0600

    add welcoming text(hadith)

--- client/src/page/homepage.jsx
+++ client/src/page/homepage.jsx
@@ -4,11 +4,13 @@ import React from "react";
 import Header from "../component/header/header.component";
 import LifePartnerSelectionNasihaCard from "../component/life-partner-selection-nasiha/life-partner-selection-nasiha-card.component";
 import Footer from "../component/footer/footer.component";
+import Banner from "../component/banner/banner.component";
 
 const Homepage = () => {
   return(
     <>
       <Header />
+      <Banner />
       <LifePartnerSelectionNasihaCard />
       <Footer />
     </>
