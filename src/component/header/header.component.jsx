ac89985ad806cc452665a42e27f05f63ac77e93b
Suman Islam <sumanislam1416@gmail.com>
2022-10-15 19:18:09 +0600

    create login page

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -19,40 +19,73 @@ const Header = () => {
   const marginRight = innerWidth > 1205 ? 'mr-30' : 'mr-5';
 
   return (
-    // white background navbar
-    <nav className="navbar navbar-expand-lg bg-white">
-      <div className="container container-fluid">
-        {/* navbar logo */}
-        <Link className={`${marginRight} navbar-brand fs-2 fw-bold purple-color`} to="/"><span className="pink-color">DoorTo</span>Nikah</Link>
-        {/* navbar toggler */}
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        {/* navbar list */}
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item fw-semibold">
-              <Link className="nav-link purple-color" aria-current="page" to="/">হোম</Link>
-            </li>
-            <li className="nav-item fw-semibold">
-              <Link className="nav-link purple-color" to="/about">আমাদের সম্পর্কে</Link>
-            </li>
-            <li className="nav-item fw-semibold">
-              <Link className="nav-link purple-color" to="/faq">প্রশ্ন ও উত্তর</Link>
-            </li>
-            <li className="nav-item fw-semibold">
-              <Link className="nav-link purple-color" to="/tutorial">টিউটোরিয়াল</Link>
-            </li>
-            <li className="nav-item fw-semibold">
-              <Link className="nav-link purple-color" to="/contact">যোগাযোগ</Link>
-            </li>
-          </ul>
-          <button type="button" className="btn btn-purple">ফ্রি রেজিষ্ট্রেশন</button>
-          <button type="button" className="btn btn-pink ml-1">লগইন</button>
-        </div>
-      </div>
-    </nav> 
-  );
+		// white background navbar
+		<nav className='navbar navbar-expand-lg bg-white'>
+			<div className='container container-fluid text'>
+				{/* navbar logo */}
+				<Link
+					className={`${marginRight} navbar-brand fs-2 fw-bold purple-color`}
+					to='/'
+				>
+					<span className='pink-color'>DoorTo</span>Nikah
+				</Link>
+				{/* navbar toggler */}
+				<button
+					className='navbar-toggler'
+					type='button'
+					data-bs-toggle='collapse'
+					data-bs-target='#navbarSupportedContent'
+					aria-controls='navbarSupportedContent'
+					aria-expanded='false'
+					aria-label='Toggle navigation'
+				>
+					<span className='navbar-toggler-icon'></span>
+				</button>
+				{/* navbar list */}
+				<div className='collapse navbar-collapse' id='navbarSupportedContent'>
+					<ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+						<li className='nav-item fw-semibold'>
+							<Link
+								className='nav-link purple-color'
+								aria-current='page'
+								to='/'
+							>
+								হোম
+							</Link>
+						</li>
+						<li className='nav-item fw-semibold'>
+							<Link className='nav-link purple-color' to='/about'>
+								আমাদের সম্পর্কে
+							</Link>
+						</li>
+						<li className='nav-item fw-semibold'>
+							<Link className='nav-link purple-color' to='/faq'>
+								প্রশ্ন ও উত্তর
+							</Link>
+						</li>
+						<li className='nav-item fw-semibold'>
+							<Link className='nav-link purple-color' to='/tutorial'>
+								টিউটোরিয়াল
+							</Link>
+						</li>
+						<li className='nav-item fw-semibold'>
+							<Link className='nav-link purple-color' to='/contact'>
+								যোগাযোগ
+							</Link>
+						</li>
+					</ul>
+					<button type='button' className='btn btn-purple'>
+						ফ্রি রেজিষ্ট্রেশন
+					</button>
+					<button type='button' className='btn btn-pink ml-1'>
+						<Link className='nav-link purple-color' to='/login'>
+							লগইন
+						</Link>
+					</button>
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Header;
