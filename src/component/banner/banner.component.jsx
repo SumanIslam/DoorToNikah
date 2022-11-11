1e42231890ae48846b95f1f4b04a2fc6310e718a
Suman Islam <sumanislam1416@gmail.com>
2022-11-11 09:59:24 +0600

    make homepage and about page responsive

--- src/component/banner/banner.component.jsx
+++ src/component/banner/banner.component.jsx
@@ -11,15 +11,17 @@ const Banner = () => (
 	<div className='banner bg-purple'>
 		<div className='container'>
 			<div className='row'>
-				<div className='col-6 mt-5 mb-5 d-flex justify-content-center align-items-center'>
+				<div className='col-12 col-md-6 col-lg-6 mt-5 mb-md-5 d-flex justify-content-center align-items-center'>
 					<BiodataSearchBox />
 				</div>
-				<div className='col-6 mt-5 mb-5 d-flex flex-column justify-content-center align-items-center'>
+				<div className='col-12 col-md-6 col-lg-6 mt-5 mb-5 d-flex flex-column justify-content-center align-items-center'>
 					<h1 className='text-white text-center mb-4'>
 						যে ব্যক্তি বিয়ে করলো সে তার অর্ধেক দ্বীন পূর্ণ করে ফেললো। বাকি
 						অর্ধেকের জন্য সে আল্লাহকে ভয় করুক।
 					</h1>
-					<span className="hadith-reference text-white">বায়হাকী, শু’আবুল ঈমান – ৫৪৮৬</span>
+					<span className='hadith-reference text-white'>
+						বায়হাকী, শু’আবুল ঈমান – ৫৪৮৬
+					</span>
 				</div>
 			</div>
 		</div>
