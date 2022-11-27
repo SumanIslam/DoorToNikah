90c608f13276a780b2eb5203fc82fb7cbd1c07ee
Suman Islam <sumanislam1416@gmail.com>
2022-11-27 14:19:53 +0600

    step6 page is completed

--- src/page/registration/registration-page6.component.jsx
+++ src/page/registration/registration-page6.component.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 // component
 import Header from '../../component/header/header.component';
 import Footer from '../../component/footer/footer.component';
+import Step6 from '../../component/registration/step6/step6.component';
 
 const RegistrationPage6 = () => {
 	return (
 		<>
 			<Header />
+      <Step6 />
 			<Footer />
 		</>
 	);
