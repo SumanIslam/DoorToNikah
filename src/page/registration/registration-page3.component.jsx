0adbf37691ec8bbc3503791f499b689dcb904d61
Suman Islam <sumanislam1416@gmail.com>
2022-11-27 08:51:09 +0600

    add registration page 3 component

---
+++ src/page/registration/registration-page3.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// component
+import Header from '../../component/header/header.component';
+import Footer from '../../component/footer/footer.component';
+
+const RegistrationPage3 = () => {
+	return (
+		<>
+			<Header />
+			<Footer />
+		</>
+	);
+};
+
+export default RegistrationPage3;
