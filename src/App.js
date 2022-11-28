e67e0bac0b82f9e9a9aa3ea0650dafb120efc4cb
Suman Islam <sumanislam1416@gmail.com>
2022-11-28 12:16:39 +0600

    step11 page is initiated

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ import RegistrationPage7 from './page/registration/registration-page7.component'
 import RegistrationPage8 from './page/registration/registration-page8.component';
 import RegistrationPage9 from './page/registration/registration-page9.component';
 import RegistrationPage10 from './page/registration/registration-page10.component';
+import RegistrationPage11 from './page/registration/registration-page11.component';
 
 // require auth
 import RequireAuth from './services/requireAuth';
@@ -51,6 +52,7 @@ function App() {
 				<Route path='registration/step8' element={<RegistrationPage8 />} />
 				<Route path='registration/step9' element={<RegistrationPage9 />} />
 				<Route path='registration/step10' element={<RegistrationPage10 />} />
+				<Route path='registration/step11' element={<RegistrationPage11 />} />
 			</Route>
 		</Routes>
 	);
---
+++ src/page/registration/registration-page11.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// component
+import Header from '../../component/header/header.component';
+import Footer from '../../component/footer/footer.component';
+import Step11 from '../../component/registration/step11/step11.component';
+
+const RegistrationPage11 = () => {
+	return (
+		<>
+			<Header />
+			<Step11 />
+			<Footer />
+		</>
+	);
+};
+
+export default RegistrationPage11;
