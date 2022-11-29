09021e72523b56f64498e397f76f7452f463979b
Suman Islam <sumanislam1416@gmail.com>
2022-11-29 20:18:32 +0600

    refactor the code of step6 component

--- src/component/registration/step6/step6-form-container.component.jsx
+++ src/component/registration/step6/step6-form-container.component.jsx
@@ -1,10 +1,35 @@
+import { useState } from 'react';
 // component
 import FormContainerButtonNav from '../common-component/form-container-button-nav/form-container-button-nav.component';
 import FormContainerNav from '../common-component/form-container-nav/form-container-nav.component';
-import NextButton from '../common-component/next-button/next-button.component';
-import SaveChangesButton from '../common-component/save-changes-button/save-changes-button.component';
+import FormButtonContainer from '../common-component/form-button-container/form-button-container.component';
+import InputField from '../common-component/input-field/input-field.component';
 
 const Step6FormContainer = () => {
+	const [personalInfo, setPersonalInfo] = useState({
+		dailyPrayer: '',
+		prayingSince: '',
+		MaintainMahram: '',
+		QuranRecitation: '',
+		majhab: '',
+		politicalViews: '',
+		watchFilm: '',
+		mentalProblem: '',
+		deeniMehnot: '',
+		pirMuridan: '',
+		beliefsAboutMazar: '',
+		favoriteIslamicBooks: '',
+		favoriteIslamicScholar: '',
+		specialSkills: '',
+		aboutYourself: '',
+	});
+
+	const handlePersonalInfo = (e) => {
+		e.preventDefault();
+		setPersonalInfo({ ...personalInfo, [e.target.name]: e.target.value });
+	};
+
+	console.log(personalInfo);
 	return (
 		<div className='step-container'>
 			<FormContainerNav />
@@ -12,142 +37,161 @@ const Step6FormContainer = () => {
 				<FormContainerButtonNav current={6} />
 				<div className='form-container'>
 					{/* for prayers */}
-					<fieldset className='border pl-1 custom-input-container mt-1'>
-						<legend className='float-none w-auto'>
-							প্রতিদিন পাঁচ ওয়াক্ত নামাজ পড়া হয়?*
-						</legend>
-						<input type='text' className='full-width' />
-					</fieldset>
-
+					<InputField
+						title='প্রতিদিন পাঁচ ওয়াক্ত নামাজ পড়া হয়?*'
+						variant='input'
+						value={personalInfo.dailyPrayer}
+						required={true}
+						name='dailyPrayer'
+						handleChange={handlePersonalInfo}
+					/>
 					{/* for prayers */}
-					<fieldset className='border pl-1 custom-input-container mt-1'>
-						<legend className='float-none w-auto'>
-							নিয়মিত নামায কত সময় যাবত পড়ছেন?*
-						</legend>
-						<input type='text' className='full-width' />
-						<p className='guide-text'>
-							অর্থাৎ কয় বছর/মাস যাবত ৫ ওয়াক্ত নামায শুরু করেছেন।
-						</p>
-					</fieldset>
+					<InputField
+						title='নিয়মিত নামায কত সময় যাবত পড়ছেন?*'
+						variant='input'
+						value={personalInfo.prayingSince}
+						required={true}
+						name='prayingSince'
+						guideText='অর্থাৎ কয় বছর/মাস যাবত ৫ ওয়াক্ত নামায শুরু করেছেন।'
+						handleChange={handlePersonalInfo}
+					/>
 
-					{/* for mahram - nonmahram */}
-					<fieldset className='border pl-1 custom-input-container mt-1'>
-						<legend className='float-none w-auto'>
-							মাহরাম/নন-মাহরাম মেনে চলেন কি?*
-						</legend>
-						<input type='text' className='full-width' />
-					</fieldset>
+					{/* for mahram - nonmahram maintain */}
+					<InputField
+						title='মাহরাম/নন-মাহরাম মেনে চলেন কি?*'
+						variant='input'
+						value={personalInfo.MaintainMahram}
+						required={true}
+						name='MaintainMahram'
+						handleChange={handlePersonalInfo}
+					/>
 
 					{/* for Quran recitation */}
-					<fieldset className='border pl-1 custom-input-container mt-1'>
-						<legend className='float-none w-auto'>
-							শুদ্ধভাবে কুরআন তিলওয়াত করতে পারেন?*
-						</legend>
-						<input type='text' className='full-width' />
-					</fieldset>
+					<InputField
+						title='শুদ্ধভাবে কুরআন তিলওয়াত করতে পারেন?*'
+						variant='input'
+						value={personalInfo.QuranRecitation}
+						required={true}
+						name='QuranRecitation'
+						handleChange={handlePersonalInfo}
+					/>
 
 					{/* for majhhab */}
-					<fieldset className='border pl-1 custom-input-container mt-1'>
-						<legend className='float-none w-auto'>
-							কোন মাঝহাব অনুসরণ করেন?*
-						</legend>
-						<input type='text' className='full-width' />
-					</fieldset>
+					<InputField
+						title='কোন মাঝহাব অনুসরণ করেন?*'
+						variant='input'
+						value={personalInfo.majhab}
+						required={true}
+						name='majhab'
+						handleChange={handlePersonalInfo}
+					/>
 
 					{/* for political views */}
-					<fieldset className='border pl-1 custom-input-container mt-1'>
-						<legend className='float-none w-auto'>
-							কোনো রাজনৈতিক দর্শন থাকলে লিখুন*
-						</legend>
-						<input type='text' className='full-width' />
-					</fieldset>
+					<InputField
+						title='কোনো রাজনৈতিক দর্শন থাকলে লিখুন*'
+						variant='input'
+						value={personalInfo.politicalViews}
+						required={true}
+						name='politicalViews'
+						handleChange={handlePersonalInfo}
+					/>
 
 					{/* for natok / serial */}
-					<fieldset className='border pl-1 custom-input-container mt-1'>
-						<legend className='float-none w-auto'>
-							নাটক/সিনেমা/সিরিয়াল/গান এসব দেখেন বা শুনেন?*
-						</legend>
-						<input type='text' className='full-width' />
-					</fieldset>
+					<InputField
+						title='নাটক/সিনেমা/সিরিয়াল/গান এসব দেখেন বা শুনেন?*'
+						variant='input'
+						value={personalInfo.watchFilm}
+						required={true}
+						name='watchFilm'
+						handleChange={handlePersonalInfo}
+					/>
 
 					{/* for physical and mental problem */}
-					<fieldset className='border pl-1 custom-input-container mt-1'>
-						<legend className='float-none w-auto'>
-							মানসিক বা শারীরিক কোনো রোগ আছে কি?*
-						</legend>
-						<input type='text' className='full-width' />
-					</fieldset>
+					<InputField
+						title='মানসিক বা শারীরিক কোনো রোগ আছে কি?*'
+						variant='input'
+						value={personalInfo.mentalProblem}
+						required={true}
+						name='mentalProblem'
+						handleChange={handlePersonalInfo}
+					/>
 
 					{/* for diner mehnot */}
-					<fieldset className='border pl-1 custom-input-container mt-1'>
-						<legend className='float-none w-auto'>
-							দ্বীনের কোন বিশেষ মেহনতে যুক্ত আছেন?*
-						</legend>
-						<input type='text' className='full-width' />
-						<p className='guide-text'>যেমনঃ তাবলীগ ইত্যাদি।</p>
-					</fieldset>
+					<InputField
+						title='দ্বীনের কোন বিশেষ মেহনতে যুক্ত আছেন?*'
+						variant='input'
+						value={personalInfo.deeniMehnot}
+						required={true}
+						name='deeniMehnot'
+						guideText='যেমনঃ তাবলীগ ইত্যাদি।'
+						handleChange={handlePersonalInfo}
+					/>
 
 					{/* for pirer murid */}
-					<fieldset className='border pl-1 custom-input-container mt-1'>
-						<legend className='float-none w-auto'>
-							আপনি কি কোনো পীরের মুরিদ বা অনুসারী?*
-						</legend>
-						<input type='text' className='full-width' />
-						<p className='guide-text'>
-							যদি হয়ে থাকেন তাহলে পীরের নাম এবং তরিকার নাম লিখবেন।{' '}
-						</p>
-					</fieldset>
+					<InputField
+						title='আপনি কি কোনো পীরের মুরিদ বা অনুসারী?*'
+						variant='input'
+						value={personalInfo.pirMuridan}
+						required={true}
+						name='pirMuridan'
+						guideText='যদি হয়ে থাকেন তাহলে পীরের নাম এবং তরিকার নাম লিখবেন।'
+						handleChange={handlePersonalInfo}
+					/>
 
-					{/* for mazar */}
-					<fieldset className='border pl-1 custom-input-container mt-1'>
-						<legend className='float-none w-auto'>
-							মাজার সম্পর্কে আপনার ধারণা বা বিশ্বাস কি?*
-						</legend>
-						<input type='text' className='full-width' />
-					</fieldset>
+					{/* for beliefs about mazar */}
+					<InputField
+						title='মাজার সম্পর্কে আপনার ধারণা বা বিশ্বাস কি?*'
+						variant='input'
+						value={personalInfo.beliefsAboutMazar}
+						required={true}
+						name='beliefsAboutMazar'
+						handleChange={handlePersonalInfo}
+					/>
 
-					{/* for islami books */}
-					<fieldset className='border pl-1 custom-input-container mt-1'>
-						<legend className='float-none w-auto'>
-							আপনার পছন্দের অন্তত ৩ টি ইসলামী বই এর নাম লিখুন*
-						</legend>
-						<input type='text' className='full-width' />
-					</fieldset>
+					{/* for favorite islamic books */}
+					<InputField
+						title='আপনার পছন্দের অন্তত ৩ টি ইসলামী বই এর নাম লিখুন*'
+						variant='input'
+						value={personalInfo.favoriteIslamicBooks}
+						required={true}
+						name='favoriteIslamicBooks'
+						handleChange={handlePersonalInfo}
+					/>
 
-					{/* for islami scholar */}
-					<fieldset className='border pl-1 custom-input-container mt-1'>
-						<legend className='float-none w-auto'>
-							আপনার পছন্দের অন্তত ৩ জন আলেমের নাম লিখুন*
-						</legend>
-						<input type='text' className='full-width' />
-					</fieldset>
+					{/* for favorite islamic scholar */}
+					<InputField
+						title='আপনার পছন্দের অন্তত ৩ জন আলেমের নাম লিখুন*'
+						variant='input'
+						value={personalInfo.favoriteIslamicScholar}
+						required={true}
+						name='favoriteIslamicScholar'
+						handleChange={handlePersonalInfo}
+					/>
 
 					{/* for Skills in Deen */}
-					<fieldset className='border pl-1 custom-input-container mt-1'>
-						<legend className='float-none w-auto'>
-							বিশেষ দ্বীনি বা দুনিয়াবি যোগ্যতা (যদি থাকে)
-						</legend>
-						<textarea rows={5} className='full-width' />
-					</fieldset>
-
+					<InputField
+						title='বিশেষ দ্বীনি বা দুনিয়াবি যোগ্যতা (যদি থাকে)'
+						variant='textarea'
+						value={personalInfo.specialSkills}
+						name='specialSkills'
+						handleChange={handlePersonalInfo}
+					/>
 					{/* for about yourself */}
-					<fieldset className='border pl-1 custom-input-container mt-1'>
-						<legend className='float-none w-auto'>
-							নিজের সম্পর্কে কিছু লিখুন
-						</legend>
-						<textarea rows={5} className='full-width' />
-						<p className='guide-text'>
-							নিজের পছন্দ-অপছন্দ, শখ-ইচ্ছা, দ্বীনী-দুনিয়াবী ইত্যাদি বিষয়
-							বিস্তারিত লিখতে হবে। কারণ এই লেখা পড়ে পাঠক আপনার সম্পর্কে সাধারণ
-							ধারণা লাভ করবে।
-						</p>
-					</fieldset>
+					<InputField
+						title='নিজের সম্পর্কে কিছু লিখুন*'
+						variant='textarea'
+						value={personalInfo.aboutYourself}
+						required={true}
+						name='aboutYourself'
+						guideText='নিজের পছন্দ-অপছন্দ, শখ-ইচ্ছা, দ্বীনী-দুনিয়াবী ইত্যাদি বিষয় বিস্তারিত লিখতে হবে। কারণ এই লেখা পড়ে পাঠক আপনার সম্পর্কে সাধারণ ধারণা লাভ করবে।'
+						handleChange={handlePersonalInfo}
+					/>
 				</div>
 				{/* buttons */}
-				<div className='d-flex justify-content-between width'>
-					<SaveChangesButton />
-					<NextButton disabled={true} />
-				</div>
+				<FormButtonContainer
+					states={personalInfo}
+					url='/biodata/registration/step7'
+				/>
 			</div>
 		</div>
 	);
