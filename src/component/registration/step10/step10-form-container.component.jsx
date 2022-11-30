60d4bba1894bfd248a9a5bd3d069c5b1922ee889
Suman Islam <sumanislam1416@gmail.com>
2022-11-30 08:40:56 +0600

    refactor the code of step10 component

--- src/component/registration/step10/step10-form-container.component.jsx
+++ src/component/registration/step10/step10-form-container.component.jsx
@@ -1,73 +1,67 @@
+import { useState } from 'react';
 // component
 import FormContainerButtonNav from '../common-component/form-container-button-nav/form-container-button-nav.component';
 import FormContainerNav from '../common-component/form-container-nav/form-container-nav.component';
-import NextButton from '../common-component/next-button/next-button.component';
-import SaveChangesButton from '../common-component/save-changes-button/save-changes-button.component';
+import FormButtonContainer from '../common-component/form-button-container/form-button-container.component';
+import SelectField from '../common-component/select-field/select-field.component';
 
 // data
 import { bool } from './data';
 
 const Step10FormContainer = () => {
+	const [authoritysAsk, setAuthoritysAsk] = useState({
+		parentsConsentAboutBiodata: '',
+		isGivenInfoLegitimate: '',
+		takeResponsibilityOfFalseInfo: ''
+	})
+
+	const handleAuthoritysAsk = (e) => {
+		e.preventDefault();
+
+		setAuthoritysAsk({...authoritysAsk, [e.target.name] : e.target.value})
+	}
+	
 	return (
 		<div className='step-container'>
 			<FormContainerNav />
 			<div className='mlr-2'>
 				<FormContainerButtonNav current={10} />
 				<div className='form-container'>
-					{/* for parents consent */}
-					<fieldset className='border pl-1 custom-input-container mt-1'>
-						<legend className='float-none w-auto'>
-							OrdhekDeen.com ওয়েবসাইটে বায়োডাটা জমা দিচ্ছেন তা অভিভাবক জানেন?*
-						</legend>
-						<select>
-							<option defaultValue={bool.selected}>{bool.selected}</option>
-							{bool.options.map((item) => (
-								<option key={item} value={item}>
-									{' '}
-									{item}{' '}
-								</option>
-							))}
-						</select>
-					</fieldset>
+					{/* parents consent about biodata*/}
+					<SelectField
+						title='OrdhekDeen.com ওয়েবসাইটে বায়োডাটা জমা দিচ্ছেন তা অভিভাবক জানেন?*'
+						name='parentsConsentAboutBiodata'
+						required={true}
+						selected={bool.selected}
+						options={bool.options}
+						handleSelect={handleAuthoritysAsk}
+					/>
 
-					{/* for promise */}
-					<fieldset className='border pl-1 custom-input-container mt-1'>
-						<legend className='float-none w-auto'>
-							আল্লাহ'র শপথ করে সাক্ষ্য দিন, যে তথ্যগুলো দিচ্ছেন সব সত্য?*
-						</legend>
-						<select>
-							<option defaultValue={bool.selected}>{bool.selected}</option>
-							{bool.options.map((item) => (
-								<option key={item} value={item}>
-									{' '}
-									{item}{' '}
-								</option>
-							))}
-						</select>
-					</fieldset>
+					{/* is given info is legitimate */}
+					<SelectField
+						title="আল্লাহ'র শপথ করে সাক্ষ্য দিন, যে তথ্যগুলো দিচ্ছেন সব সত্য?*"
+						name='isGivenInfoLegitimate'
+						required={true}
+						selected={bool.selected}
+						options={bool.options}
+						handleSelect={handleAuthoritysAsk}
+					/>
 
-					{/* for promise */}
-					<fieldset className='border pl-1 custom-input-container mt-1'>
-						<legend className='float-none w-auto'>
-							কোনো মিথ্যা তথ্য দিয়ে থাকলে তার দুনিয়াবী ও আখিরাতের দায়ভার
-							ওয়েবসাইট কর্তৃপক্ষ নিবে না। আপনি কি রাজি?*
-						</legend>
-						<select>
-							<option defaultValue={bool.selected}>{bool.selected}</option>
-							{bool.options.map((item) => (
-								<option key={item} value={item}>
-									{' '}
-									{item}{' '}
-								</option>
-							))}
-						</select>
-					</fieldset>
+					{/* take responsibility for given info */}
+					<SelectField
+						title='কোনো মিথ্যা তথ্য দিয়ে থাকলে তার দুনিয়াবী ও আখিরাতের দায়ভার ওয়েবসাইট কর্তৃপক্ষ নিবে না। আপনি কি রাজি?*'
+						name='takeResponsibilityOfFalseInfo'
+						required={true}
+						selected={bool.selected}
+						options={bool.options}
+						handleSelect={handleAuthoritysAsk}
+					/>
 				</div>
 				{/* buttons */}
-				<div className='d-flex justify-content-between width'>
-					<SaveChangesButton />
-					<NextButton disabled={true} />
-				</div>
+				<FormButtonContainer
+					states={authoritysAsk}
+					url='/biodata/registration/step11'
+				/>
 			</div>
 		</div>
 	);
