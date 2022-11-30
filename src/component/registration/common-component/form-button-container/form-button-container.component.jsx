9bcdd9c379ac663355b22ab5d98a39e73232443b
Suman Islam <sumanislam1416@gmail.com>
2022-11-30 16:43:34 +0600

    update FormButtonContainer

--- src/component/registration/common-component/form-button-container/form-button-container.component.jsx
+++ src/component/registration/common-component/form-button-container/form-button-container.component.jsx
@@ -1,13 +1,68 @@
-import SaveChangesButton from "../save-changes-button/save-changes-button.component";
-import NextButton from "../next-button/next-button.component";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+// registration context
+import useRegistration from '../../../../hooks/useRegistration';
 
-const FormButtonContainer = () => {
+// mui
+import DoubleArrowIcon from '@mui/icons-material/DoubleArrow';
+import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
+
+// styles
+import './form-button-container.style.scss';
+import '../../../../styles/utils.scss'
+
+const FormButtonContainer = ({ states, nextUrl, backUrl }) => {
+  const [loading, setLoading] = useState(false);
+  const [saved, setSaved] = useState(true);
+
+  const { candidatesInfo, setCandidatesInfo } = useRegistration();
+
+  // console.log(states);
+  // console.log(candidatesInfo);
   return (
-    <div className='d-flex justify-content-between width'>
-      <SaveChangesButton />
-      <NextButton disabled={true} />
-    </div>
-  )
+		<div className='d-flex justify-content-between width'>
+			{loading ? (
+				<button class='small-button' type='button' disabled>
+					<span
+						className='spinner-border spinner-border-sm'
+						role='status'
+						aria-hidden='true'
+					></span>{' '}
+					saving...
+				</button>
+			) : saved ? (
+				<button className='small-button'>Saved</button>
+			) : (
+				<button className='small-button'>Save Changes</button>
+			)}
+			<div>
+				{backUrl && (
+					<button className='next-button'>
+						<Link to={backUrl}>
+							<KeyboardDoubleArrowLeftIcon /> Back
+						</Link>
+					</button>
+				)}
+				{!saved ? (
+					<button
+						className='disabled-button'
+						disabled
+						title='Save the changes first'
+					>
+						<Link to={nextUrl}>
+							Next <DoubleArrowIcon />
+						</Link>
+					</button>
+				) : (
+					<button className='next-button'>
+						<Link to={nextUrl}>
+							Next <DoubleArrowIcon />
+						</Link>
+					</button>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default FormButtonContainer;
