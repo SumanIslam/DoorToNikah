2779781f6ccfd964ab4e0a06dd7099cdd88097e8
Suman Islam <sumanislam1416@gmail.com>
2022-12-03 10:46:14 +0600

    set autocomplete off in input or textarea tag

--- src/component/registration/common-component/input-field/input-field.component.jsx
+++ src/component/registration/common-component/input-field/input-field.component.jsx
@@ -5,9 +5,25 @@ const InputField = ({title, guideText, variant, value, name, required, handleCha
 		<fieldset className='border pl-1 custom-input-container mt-1'>
 			<legend className='float-none w-auto'>{title}</legend>
 			{variant === 'input' ? (
-				<input type='text' name={name}  value={value} className='full-width' onChange={handleChange} required={required} />
+				<input
+					type='text'
+					name={name}
+					value={value}
+					className='full-width'
+					onChange={handleChange}
+					required={required}
+					autoComplete='off'
+				/>
 			) : (
-				<textarea rows={5} name={name}  value={value} className='full-width' onChange={handleChange} required={required} />
+				<textarea
+					rows={5}
+					name={name}
+					value={value}
+					className='full-width'
+					onChange={handleChange}
+					required={required}
+					autoComplete='off'
+				/>
 			)}
 			{guideText && <p className='guide-text'>{guideText}</p>}
 		</fieldset>
