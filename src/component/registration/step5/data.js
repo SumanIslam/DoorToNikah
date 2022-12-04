efce1a87dd2f738e33b1213475d4a156621fd083
Suman Islam <sumanislam1416@gmail.com>
2022-12-04 23:04:17 +0600

    step5 completed

--- src/component/registration/step5/data.js
+++ src/component/registration/step5/data.js
@@ -1,10 +1,16 @@
 export const numberOfSisters = {
-	title: 'বোন কয়জন?',
-	selected: 'বোন নেই',
-	options: ['১', '২', '৩', '৪', '৫', '৬', '৭', '৮', '৯', '১০'],
+	title: 'বোন কয়জন?*',
+	selected: {
+		value: '',
+		option: '----'
+	},
+	options: ['বোন নেই','১', '২', '৩', '৪', '৫', '৬', '৭', '৮', '৯', '১০'],
 };
 export const numberOfBrothers = {
-	title: 'ভাই কয়জন?',
-	selected: 'ভাই নেই',
-	options: ['১', '২', '৩', '৪', '৫', '৬', '৭', '৮', '৯', '১০'],
+	title: 'ভাই কয়জন?*',
+	selected: {
+		value: '',
+		option: '----',
+	},
+	options: ['ভাই নেই','১', '২', '৩', '৪', '৫', '৬', '৭', '৮', '৯', '১০'],
 };
