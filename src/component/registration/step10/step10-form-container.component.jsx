0247407d720499e83af7b35f431f862ecab142a0
Suman Islam <sumanislam1416@gmail.com>
2022-12-05 10:17:41 +0600

    complete step 8,9,10,11

--- src/component/registration/step10/step10-form-container.component.jsx
+++ src/component/registration/step10/step10-form-container.component.jsx
@@ -5,6 +5,9 @@ import FormContainerNav from '../common-component/form-container-nav/form-contai
 import FormButtonContainer from '../common-component/form-button-container/form-button-container.component';
 import SelectField from '../common-component/select-field/select-field.component';
 
+// registration context
+import useRegistration from '../../../hooks/useRegistration';
+
 // data
 import { bool } from './data';
 
@@ -12,56 +15,79 @@ const Step10FormContainer = () => {
 	const [authoritysAsk, setAuthoritysAsk] = useState({
 		parentsConsentAboutBiodata: '',
 		isGivenInfoLegitimate: '',
-		takeResponsibilityOfFalseInfo: ''
-	})
+		takeResponsibilityOfFalseInfo: '',
+	});
+
+	// registration context
+	const { candidatesInfo, setCandidatesInfo } = useRegistration();
+
+	const [loading, setLoading] = useState(false);
+	const [saved, setSaved] = useState(false);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setLoading(true);
+		setCandidatesInfo({
+			...candidatesInfo,
+			authoritysAsk: { ...authoritysAsk },
+		});
+		setTimeout(() => {
+			setLoading(false);
+			setSaved(true);
+		}, 2000);
+	};
 
 	const handleAuthoritysAsk = (e) => {
 		e.preventDefault();
 
-		setAuthoritysAsk({...authoritysAsk, [e.target.name] : e.target.value})
-	}
-	
+		setAuthoritysAsk({ ...authoritysAsk, [e.target.name]: e.target.value });
+	};
+
 	return (
 		<div className='step-container'>
 			<FormContainerNav />
 			<div className='mlr-2'>
 				<FormContainerButtonNav current={10} />
 				<div className='form-container'>
-					{/* parents consent about biodata*/}
-					<SelectField
-						title='OrdhekDeen.com ওয়েবসাইটে বায়োডাটা জমা দিচ্ছেন তা অভিভাবক জানেন?*'
-						name='parentsConsentAboutBiodata'
-						required={true}
-						selected={bool.selected}
-						options={bool.options}
-						handleSelect={handleAuthoritysAsk}
-					/>
+					<form onSubmit={handleSubmit}>
+						{/* parents consent about biodata*/}
+						<SelectField
+							title='OrdhekDeen.com ওয়েবসাইটে বায়োডাটা জমা দিচ্ছেন তা অভিভাবক জানেন?*'
+							name='parentsConsentAboutBiodata'
+							required={true}
+							selected={bool.selected}
+							options={bool.options}
+							handleSelect={handleAuthoritysAsk}
+						/>
 
-					{/* is given info is legitimate */}
-					<SelectField
-						title="আল্লাহ'র শপথ করে সাক্ষ্য দিন, যে তথ্যগুলো দিচ্ছেন সব সত্য?*"
-						name='isGivenInfoLegitimate'
-						required={true}
-						selected={bool.selected}
-						options={bool.options}
-						handleSelect={handleAuthoritysAsk}
-					/>
+						{/* is given info is legitimate */}
+						<SelectField
+							title="আল্লাহ'র শপথ করে সাক্ষ্য দিন, যে তথ্যগুলো দিচ্ছেন সব সত্য?*"
+							name='isGivenInfoLegitimate'
+							required={true}
+							selected={bool.selected}
+							options={bool.options}
+							handleSelect={handleAuthoritysAsk}
+						/>
 
-					{/* take responsibility for given info */}
-					<SelectField
-						title='কোনো মিথ্যা তথ্য দিয়ে থাকলে তার দুনিয়াবী ও আখিরাতের দায়ভার ওয়েবসাইট কর্তৃপক্ষ নিবে না। আপনি কি রাজি?*'
-						name='takeResponsibilityOfFalseInfo'
-						required={true}
-						selected={bool.selected}
-						options={bool.options}
-						handleSelect={handleAuthoritysAsk}
-					/>
+						{/* take responsibility for given info */}
+						<SelectField
+							title='কোনো মিথ্যা তথ্য দিয়ে থাকলে তার দুনিয়াবী ও আখিরাতের দায়ভার ওয়েবসাইট কর্তৃপক্ষ নিবে না। আপনি কি রাজি?*'
+							name='takeResponsibilityOfFalseInfo'
+							required={true}
+							selected={bool.selected}
+							options={bool.options}
+							handleSelect={handleAuthoritysAsk}
+						/>
+						{/* buttons */}
+						<FormButtonContainer
+							nextUrl='/biodata/registration/step9'
+							backUrl='/biodata/registration/step7'
+							loading={loading}
+							saved={saved}
+						/>
+					</form>
 				</div>
-				{/* buttons */}
-				<FormButtonContainer
-					states={authoritysAsk}
-					url='/biodata/registration/step11'
-				/>
 			</div>
 		</div>
 	);
