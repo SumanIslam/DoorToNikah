bc6f65f2a813a9149c0d04bbc9a2c438e317178a
Suman Islam <sumanislam1416@gmail.com>
2022-12-09 18:44:16 +0600

    completed profile page

--- src/component/profile/profile.component.jsx
+++ src/component/profile/profile.component.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 
+// component
+import BioProfileContainer from '../registration/common-component/bio-profile-container/bio-profile-container.component';
+import BioInfoContainer from '../registration/common-component/bio-info-container/bio-info-container.component';
+
+// registration context
+import useRegistration from '../../hooks/useRegistration';
+
+// style
+import './profile.style.scss'
+
 const Profile = () => {
+  const { candidatesInfo } = useRegistration();
+  // const biodataToShowArray = [
+	// 	candidatesInfo.candidatesName,
+	// 	candidatesInfo.address,
+	// 	candidatesInfo.educationalQualification,
+	// 	candidatesInfo.familyInfo,
+	// 	candidatesInfo.personalInfo,
+	// 	candidatesInfo.marriageInfo,
+	// 	candidatesInfo.otherInfo,
+	// 	candidatesInfo.partnersCharacteristics,
+	// 	candidatesInfo.authoritysAsk,
+	// ];
   return (
-    <div>Profile</div>
-  )
+		<div className='biodata-profile'>
+			<BioProfileContainer
+				biodataId={candidatesInfo?.biodataId}
+				generalInfo={candidatesInfo.generalInfo}
+			/>
+			<BioInfoContainer candidatesInfo={candidatesInfo} />
+		</div>
+	);
 }
 
 export default Profile;
--- src/page/profile-page.jsx
+++ src/page/profile-page.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
 // component
+import Header from '../component/header/header.component';
+import Footer from '../component/footer/footer.component'
 import Profile from '../component/profile/profile.component';
 
 const ProfilePage = () => {
   return (
-    <Profile />
-  )
+		<>
+			<Header />
+			<Profile />
+      <Footer />
+		</>
+	);
 }
 
 export default ProfilePage;
