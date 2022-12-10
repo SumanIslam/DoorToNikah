abcc0194c2ba92bb09eeee2ec29636a2770fa03e
imsamrat <imsamrat24@gmail.com>
2022-12-10 22:15:33 +0600

    Admin Panel Navbar Added

---
+++ src/component/manage-biodata/manageBiodata.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageBiodataComponent = () => {
+  return (
+    <div>
+      <h3>this is Manage BioData Page</h3>
+    </div>
+  );
+};
+
+export default ManageBiodataComponent;
---
+++ src/component/manage-biodata/totalBiodata.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TotalBiodata = () => {
+  return (
+    <div>
+      <h1>This is Total BioData Page</h1>
+    </div>
+  );
+};
+
+export default TotalBiodata;
