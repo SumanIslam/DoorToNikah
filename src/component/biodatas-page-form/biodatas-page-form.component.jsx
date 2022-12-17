f5565de457d40597e2c42e7e6e4e0fe7786bb036
Suman Islam <sumanislam1416@gmail.com>
2022-12-17 22:43:59 +0600

    updated biodatas-page-form

--- src/component/biodatas-page-form/biodatas-page-form.component.jsx
+++ src/component/biodatas-page-form/biodatas-page-form.component.jsx
@@ -51,13 +51,28 @@ const BiodatasPageForm = () => {
 				searchData,
 				page
 			);
-			setBiodatas(biodatasWithPagination);
+
+			if(biodatasWithPagination?.length) {
+				setBiodatas(biodatasWithPagination);
+			} else if(biodatasWithPagination?.date) {
+				setBiodatas(biodatasWithPagination);
+			} else {
+				setBiodatas(null)
+			}
+			
 			setIsLoading(true);
 			setTimeout(() => {
 				setIsLoading(false);
 			}, 2000);
 			const biodatas = await httpGETBiodatas(searchData);
-			setCount(Math.ceil(biodatas.length / 12));
+			if (biodatas?.length) {
+				setCount(Math.ceil(biodatas.length / 12));
+			} else if (biodatasWithPagination?.date) {
+				setCount(0)
+			} else {
+				setCount(0);
+			}
+			
 		} catch(err) {
 			console.log(err);
 		}
@@ -69,11 +84,22 @@ const BiodatasPageForm = () => {
 				searchData,
 				page
 			);
-			setBiodatas(biodatasWithPagination);
-			if (count === 10) {
-				const biodatas = await httpGETBiodatas(searchData);
-				setCount(Math.ceil(biodatas.length / 12));
+			if (biodatasWithPagination?.length) {
+				setBiodatas(biodatasWithPagination);
+			} else if (biodatasWithPagination?.date) {
+				setBiodatas(biodatasWithPagination);
+			} else {
+				setBiodatas(null);
 			}
+
+				const biodatas = await httpGETBiodatas(searchData);
+				if (biodatas?.length) {
+					setCount(Math.ceil(biodatas.length / 12));
+				} else if (biodatasWithPagination?.date) {
+					setCount(0);
+				} else {
+					setCount(0);
+				}
 		}
 		getBiodatas();
 	}, [page])
