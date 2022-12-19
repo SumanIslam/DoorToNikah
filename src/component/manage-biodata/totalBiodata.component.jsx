2626f837a2c216d98e9fac3a9226e0e342f1882a
Suman Islam <sumanislam1416@gmail.com>
2022-12-19 13:05:20 +0600

    add some more apis for admin panel

--- src/component/manage-biodata/totalBiodata.component.jsx
+++ src/component/manage-biodata/totalBiodata.component.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+// components
+import BiodatasPageForm from '../biodatas-page-form/biodatas-page-form.component';
+import BiodatasShowcase from '../biodatas-showcase/biodatas-showcase.component';
+
 const TotalBiodata = () => {
   return (
     <div>
-      <h1>This is Total BioData Page</h1>
+      <BiodatasPageForm admin={true} />
+      <BiodatasShowcase admin={true} />
     </div>
   );
 };
