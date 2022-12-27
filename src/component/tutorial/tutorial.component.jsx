ef4af34e4e666ab01ecb7e57c6b67927cca1c116
imsamrat <imsamrat24@gmail.com>
2022-12-27 07:58:52 +0600

    Mobile video Link Added

--- src/component/tutorial/tutorial.component.jsx
+++ src/component/tutorial/tutorial.component.jsx
@@ -34,7 +34,7 @@ const Tutorial = () => {
 							<iframe
 								width='900'
 								height='515'
-								src='https://www.youtube.com/embed/PFIKdnC9aZk'
+								src='https://www.youtube.com/embed/-EnHPQ8tOg0'
 								title='YouTube video player'
 								frameBorder='0'
 								allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
